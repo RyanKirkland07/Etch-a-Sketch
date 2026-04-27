24bf3723b4ff17547aa98695afa46d886529449a
RyanKirkland07 <132314358+RyanKirkland07@users.noreply.github.com>
2026-04-27 17:49:02 +1000

    added functionality to the ui allowing for a new grid to be created with a size specified by the user

--- etchASketch.js
+++ etchASketch.js
@@ -2,6 +2,7 @@ const main = document.querySelector("body");
 
 function buildGrid(size){
     let grid = document.createElement("div");
+    grid.id = "grid";
     grid.style = "display: flex; flex-direction: column; justify-content: center; align-items: center; margin: 0; border: solid 2px black;";
     main.append(grid);
 
@@ -23,3 +24,22 @@ function hover(event){
 }
 
 buildGrid(16);
+
+const confirmBtn = document.querySelector("#confirm");
+const sizeInput = document.querySelector("#new-size");
+const infoText = document.querySelector("#info");
+
+confirmBtn.addEventListener("click", newGrid);
+
+function newGrid(){
+    let size = sizeInput.value;
+
+    if(size < 1 || size > 100){
+        infoText.textContent = "Size invalid; must be between 1 and 100";
+        return;
+    }
+    let currGrid = document.querySelector("#grid");
+    grid.remove();
+    buildGrid(size);
+    infoText.textContent = "Create new grid of size:";
+}
